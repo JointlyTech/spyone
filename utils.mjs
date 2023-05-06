e41b54f1510db0842d4c7b5d0df98e51822e1f9f
Antonio Caputo <87689+tonycaputome@users.noreply.github.com>
2023-05-06 20:43:59 +0200

    feat: remove simple-git dependency

--- utils.mjs
+++ utils.mjs
@@ -1,42 +1,41 @@
-import os from 'os';
-import git from 'simple-git';
+import { execSync } from "child_process";
+import os from "os";
 
 const EXCLUDED_FILES = [
-  'package-lock.json',
-  'package.json',
-  'yarn.lock',
-  'CHANGELOG.md',
-  'composer.lock',
-  '',
+  "package-lock.json",
+  "package.json",
+  "yarn.lock",
+  "CHANGELOG.md",
+  "composer.lock",
+  "",
 ];
 
-export const downloadRepo = (repoUrl, dir, branchName = 'main') => {
+export const downloadRepo = (repoUrl, dir, branchName = "main") => {
   return new Promise((resolve, reject) => {
-    const repoName = repoUrl.split('/').pop().replace('.git', '');
-
-    // Clone the repo in the tmp directory using simple-git
-    git(dir)
-      .clone(repoUrl, dir, ['--branch', branchName], (err) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(dir);
-        }
-      });
+    const repoName = repoUrl.split("/").pop().replace(".git", "");
+    try {
+      const cloneArgs = `git clone ${repoUrl} ${dir} --branch ${branchName}`;
+      const result = execSync(`${cloneArgs}`).toString();
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
   });
 };
-
 
 // Given a path to a directory, get the list of addition and deletion counts from the git history of each file
 export const getData = (dir, daysAmount) => {
   return new Promise(async (resolve, reject) => {
-    console.log('Getting repo history...')
+    console.log("Getting repo history...");
     getRepoHistory(dir, daysAmount).then((repoHistory) => {
-      const files = repoHistory.split(os.EOL).map((line) => {
-        return line.split('\t').pop();
-      }).filter((file) => {
-        return EXCLUDED_FILES.includes(file) === false;
-      });
+      const files = repoHistory
+        .split(os.EOL)
+        .map((line) => {
+          return line.split("\t").pop();
+        })
+        .filter((file) => {
+          return EXCLUDED_FILES.includes(file) === false;
+        });
 
       const uniqueFiles = [...new Set(files)];
 
@@ -47,7 +46,7 @@ export const getData = (dir, daysAmount) => {
         // Get the list of addition and deletion counts
         const counts = result.split(os.EOL).map((line) => {
           if (line.length === 0) return { additions: 0, deletions: 0 };
-          const [additions, deletions] = line.split('\t');
+          const [additions, deletions] = line.split("\t");
           return {
             additions: parseInt(additions),
             deletions: parseInt(deletions),
@@ -55,12 +54,15 @@ export const getData = (dir, daysAmount) => {
         });
 
         // Sum the addition and deletion counts
-        const total = counts.reduce((prev, curr) => {
-          return {
-            additions: prev.additions + curr.additions,
-            deletions: prev.deletions + curr.deletions,
-          };
-        }, { additions: 0, deletions: 0 });
+        const total = counts.reduce(
+          (prev, curr) => {
+            return {
+              additions: prev.additions + curr.additions,
+              deletions: prev.deletions + curr.deletions,
+            };
+          },
+          { additions: 0, deletions: 0 }
+        );
         fileData.set(file, {
           additions: total.additions || 0,
           deletions: total.deletions || 0,
@@ -72,23 +74,24 @@ export const getData = (dir, daysAmount) => {
   });
 };
 
-
-
 export const getFileHistory = (repoHistory, file) => {
-  const fileHistory = repoHistory.split(os.EOL).filter((line) => {
-    return line.split('\t').pop() === file;
-  }).join(os.EOL);
+  const fileHistory = repoHistory
+    .split(os.EOL)
+    .filter((line) => {
+      return line.split("\t").pop() === file;
+    })
+    .join(os.EOL);
   return fileHistory;
 };
 
 export const getRepoHistory = (dir, daysAmount) => {
   return new Promise((resolve, reject) => {
-    git(dir).raw(['log', '--pretty=format:', '--numstat', `--since="${daysAmount} days ago"`], (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
+    try {
+      const args = `git log --pretty=format: --numstat --since="${daysAmount} days ago"`;
+      const result = execSync(`cd ${dir} && ${args}`).toString();
+      resolve(result);
+    } catch (err) {
+      reject(err);
+    }
   });
 };
