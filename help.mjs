8dce1218fd8b3d7bfa86b6ed1500fd102cc2e582
Marco Ingraiti <marco.ingraiti@icloud.com>
2023-05-26 00:07:30 +0200

    feat: new help file

---
+++ help.mjs
@@ -0,0 +1,22 @@
+export const help = `
+Usage: npx @jointly/spyone [options]
+
+If no options are provided, the tool will run in interactive mode.
+
+Options:
+
+    The repo url to analyze
+    --repoUrl=repourl.com
+
+    The number of days to consider
+    --days=30
+
+    The name of the branch to consider
+    --branch=main
+
+    The output format
+    --output=json
+
+    The location to save the results
+    --save=results
+`;
